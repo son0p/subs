367334731aeae4ace64624be2a0e07f78f86dd9d
fede2001 <fede2001@hotmail.com>
2013-02-22 18:06:32 -0500

    intento de subs

---
+++ index.php
@@ -0,0 +1,25 @@
+
+  <html>
+        <head>
+         <script src="popcorn-complete.js"></script>
+        </head>
+        <body>
+          <video id="video" data-timeline-sources="data.srt"
+            controls
+            width='250px'
+            poster="http://tuxpaint.org/stamps/stamps/animals/birds/cartoon/tux.png">
+ 
+        
+ 
+           <source id='ogv'
+             src="http://archive.org/download/LucuOgv/bbb400p.ogv"
+             type='video/ogg; codecs="theora, vorbis"'>
+ 
+           <p>Your user agent does not support the HTML5 Video element.</p>
+ 
+         </video>
+         <div id="footnote-container"></div>
+         <div id="map-container"></div>
+         <div id="iframe-container"></div>
+       </body>
+     </html>
